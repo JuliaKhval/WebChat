4884b027bc3ac8dfc3a8fa72b3e06e74fb7ffa8f
julia <juliakhval2512@gmail.com>
2025-04-03 15:05:23 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Navbar v-if="isAuthenticated" />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import { mapState } from 'pinia'
+import { useAuthStore } from './stores/auth'
+
+export default {
+  components: { Navbar },
+  computed: {
+    ...mapState(useAuthStore, ['isAuthenticated'])
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
