742527253dd44ba6c4c246668788600a674e4205
julia <juliakhval2512@gmail.com>
2025-05-10 13:58:57 +0300

    change second url for server

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: 'http://messenger.somee.com',
+    baseURL: 'https://messengertester.somee.com',
     withCredentials: false,
     headers: {
         Accept: 'application/json',
